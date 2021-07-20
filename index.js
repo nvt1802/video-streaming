1fda1cb0cd27429c4545aa814240df72cc58dfd6
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2021-07-20 14:28:59 +0700

    update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/video", function (req, res) {
 
   // Parse Range
   // Example: "bytes=32324-"
-  const CHUNK_SIZE = 10 ** 6 // 1MB
+  const CHUNK_SIZE = 10 ** 8 // 1MB
   const start = Number(range.replace(/\D/g, ""))
   const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
 
